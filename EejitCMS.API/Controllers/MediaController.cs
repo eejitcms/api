b52863efba2bc6ff5bf1f572a778f0104eef9faf
Alekaei <alekaei522@gmail.com>
2019-02-21 19:51:13 +0000

    Added Media Endpoint structure

---
+++ EejitCMS.API/Controllers/MediaController.cs
@@ -0,0 +1,42 @@
+﻿using EejitCMS.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EejitCMS.API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class MediaController : ControllerBase
+	{
+		// POST api/media
+		[HttpPost]
+		public void Post([FromBody] string value)
+		{
+			throw new NotFiniteNumberException ();
+		}
+
+		// GET api/media/:id
+		[HttpGet("{id:string}")]
+		public Page Get(string id)
+		{
+			throw new NotFiniteNumberException ();
+		}
+
+		// PUT api/media/:id
+		[HttpPut("{id:string}")]
+		public void Put(string id, [FromBody] string value)
+		{
+			throw new NotFiniteNumberException ();
+		}
+
+		// DELETE api/media/:id
+		[HttpDelete("{id:string}")]
+		public void Delete(string id)
+		{
+			throw new NotFiniteNumberException ();
+		}
+	}
+}
---
+++ EejitCMS.API/Models/Media.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EejitCMS.API.Models
+{
+	public class Media
+	{
+		public int Id { get; set; }
+	}
+}
