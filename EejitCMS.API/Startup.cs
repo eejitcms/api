bd83f2e9e7fdb38830f38095292994198f982a02
Alekaei <alekaei522@gmail.com>
2019-02-25 20:55:51 +0000

    Migrate to .NET Core 2.2

--- EejitCMS.API/Startup.cs
+++ EejitCMS.API/Startup.cs
@@ -30,7 +30,7 @@ namespace EejitCMS.API
 			services.AddDbContext<AccountContext> (opt => opt.UseInMemoryDatabase ("Accounts"));
 			services.AddDbContext<PageContext> (opt => opt.UseInMemoryDatabase ("Pages"));
 
-			services.AddMvc ().SetCompatibilityVersion (CompatibilityVersion.Version_2_1);
+			services.AddMvc ().SetCompatibilityVersion (CompatibilityVersion.Version_2_2);
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
