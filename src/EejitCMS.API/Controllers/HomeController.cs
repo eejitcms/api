db603a79a38cf144c5b96ebd5fdc735e0a07570b
Alekaei <alekaei522@gmail.com>
2019-04-22 23:21:16 +0100

    Created Base project with AppMetrics, Serilog and AspNetCore Identity

---
+++ src/EejitCMS.API/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace EejitCMS.API.Controllers
+{
+	[Route("api/[controller]")]
+	public class HomeController : ControllerBase
+	{
+		[HttpGet]
+		public async Task<IActionResult> Index()
+		{
+			return Ok();
+		}
+	}
+}
---
+++ src/EejitCMS.API/Program.cs
@@ -0,0 +1,31 @@
+﻿using App.Metrics.AspNetCore;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Serilog;
+
+namespace EejitCMS.API
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			CreateWebHostBuilder(args).Build().Run();
+		}
+
+		public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+			WebHost.CreateDefaultBuilder(args)
+				.UseSerilog((context, config) =>
+				{
+					config.ReadFrom.Configuration(context.Configuration);
+				})
+				.ConfigureAppMetricsHostingConfiguration(options =>
+				{
+					options.AllEndpointsPort = 3333;
+					options.EnvironmentInfoEndpoint = "/env";
+					options.MetricsEndpoint = "/metrics";
+					options.MetricsTextEndpoint = "/metrics-text";
+				})
+				.UseMetrics()
+				.UseStartup<Startup>();
+	}
+}
